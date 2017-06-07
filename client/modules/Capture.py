0a2aa0450054f63a3d618861f95d30948feea807
Wesley Thompson <wesleythompson1995@gmail.com>
2017-06-07 00:19:54 -0500

    Added resolution adjustments and filepath handling

--- client/modules/Capture.py
+++ client/modules/Capture.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8-*-
 import re
+import os
 import cv2
 import sys, datetime
 import smtplib
@@ -8,15 +9,21 @@ from client import app_utils
 WORDS = ["CAPTURE"]
 
 camIndex = 0
-filepath = "/home/wesley/jasper_captures/"
+filepath = "jasper_captures/"
 imgType = ".jpg"
+preferredWidth = 1920
+preferredHeight = 1080
 
 def isValid(text):
     return bool(re.search(r'\bcapture\b', text, re.IGNORECASE))
 
 def handle(text, mic, profile):
     cam = cv2.VideoCapture(camIndex)
-    
+
+    #Adjust capture resolution
+    cam.set(3, preferredWidth)
+    cam.set(4, preferredHeight)    
+
     #Let the light levels adjust on the camera
     for i in xrange(30):
         retVal, img = cam.read()
@@ -25,8 +32,14 @@ def handle(text, mic, profile):
     mic.say("3, 2, 1, Say Cheese.")
     retval, capture = cam.read()
     
-    imagePath = "butthole" + imgType
-    cv2.imwrite(imagePath, capture)
+    try: 
+        os.makedirs(filepath)
+    except OSError:
+        if not os.path.isdir(filepath):
+            raise
+
+    imagePath = filepath + str(datetime.datetime.now()) + imgType
+    print cv2.imwrite(imagePath, capture)
     del(cam)
 
     mic.say("Looking great. Would you like me to email this photo to you?")    
